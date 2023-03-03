80004f3bd0fd12ee5ae4f6945655c4c3cdc96dd8
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-03-03 17:07:18 -0500

    Add functionality

---
+++ script.js
@@ -0,0 +1,40 @@
+const submitBtn = document.querySelector("button");
+
+submitBtn.addEventListener("click", (event) => {
+    event.stopPropagation();
+    console.log("button was clicked");
+    displayThankYou();
+});
+
+function displayThankYou() {
+    console.log("display thank you was called")
+    const ratingState = document.querySelector(".rating-state");
+    const thankYouState = document.querySelector(".thank-you-state");
+
+    ratingState.style.display = "none";
+    thankYouState.style.display = "flex";
+}
+
+const ratingOptions = document.querySelectorAll("input[name=rating");
+
+for (let i = 0; i < ratingOptions.length; i++) {
+    ratingOptions[i].parentElement.addEventListener("click", (event) => { //on the div, to increase the hit radius
+        if (!ratingOptions[i].checked) {
+            ratingOptions[i].checked = true; //in case they clicked the div but not the actual radio button
+        }
+        removeActive();
+        if (event.target.nodeName === "INPUT") {
+            event.target.parentElement.classList.add("active"); //if they hit the radio button, add class list to parent (div)
+        }
+        else {
+            event.target.classList.add("active"); //if div itself was hit
+        }
+    });
+}
+
+function removeActive() {
+    const activeOption = document.querySelector(".active");
+    if (activeOption !== null) {
+        activeOption.classList.remove("active");
+    }
+}
