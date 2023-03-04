635aa30d93c6f5b400e15db35898d687bdd7411d
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-03-04 06:27:06 -0500

    Add function to display rating

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
+let submittedRating;
+
 const submitBtn = document.querySelector("button");
 
 submitBtn.addEventListener("click", (event) => {
     event.stopPropagation();
     console.log("button was clicked");
-    displayThankYou();
+    if (submittedRating !== undefined){
+        displayThankYou();
+    }
 });
 
 function displayThankYou() {
     console.log("display thank you was called")
+    displayRating();
     const ratingState = document.querySelector(".rating-state");
     const thankYouState = document.querySelector(".thank-you-state");
 
@@ -21,6 +26,7 @@ for (let i = 0; i < ratingOptions.length; i++) {
     ratingOptions[i].parentElement.addEventListener("click", (event) => { //on the div, to increase the hit radius
         if (!ratingOptions[i].checked) {
             ratingOptions[i].checked = true; //in case they clicked the div but not the actual radio button
+            submittedRating = ratingOptions[i].value; 
         }
         removeActive();
         if (event.target.nodeName === "INPUT") {
@@ -37,4 +43,9 @@ function removeActive() {
     if (activeOption !== null) {
         activeOption.classList.remove("active");
     }
+}
+
+function displayRating() {
+    const ratingValue = document.getElementById("rating-value");
+    ratingValue.innerText = submittedRating;
 }
